60749f523b6dab8106f282d9d28030aec726ef7f
garcia <garciapdev@gmail.com>
2019-05-06 19:49:09 -0500

    both scripts work but cant change the taskbar order

--- taskbarHotkeys.sh
+++ taskbarHotkeys.sh
@@ -6,8 +6,17 @@ contains() {
 }
 
 
-app_list=$(wmctrl -lx | awk '{print $3}')
+currentDesktop=$(wmctrl -d | awk '$2=="*" {print $1}')
+app_list=$(wmctrl -lx | awk -v var="$currentDesktop" '$2==var {print $3}')
+
 if contains "$app_list" "$1";
-    then wmctrl -x -a "$1" ;
-    else ("$2"&) ;
+    then 
+        if [ "$2" == "pcmanfm" ];
+            then
+            apps=$(wmctrl -lx | awk -v var="$currentDesktop" '$2==var {print $1}')
+            appId=$(echo $apps | awk -v k="$1" '{print $k}')
+            wmctrl -i -a "$appId"
+        else wmctrl -x -a "$1"
+        fi
+    else ("$2"&)
 fi
--- unpinnedApps.sh
+++ unpinnedApps.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-app_list=$(wmctrl -lx | awk '{print $3}')
+keyNumber=$1
 
-#filter app_list for second row == 0
+#filter desktop list for second row == *
+currentDesktop=$(wmctrl -d | awk '$2=="*" {print $1}')
+app_list=$(wmctrl -lx | awk -v var="$currentDesktop" '$2==var {print $1}')
+
+appId=$(echo $app_list | awk -v k="$keyNumber" '{print $k}')
+
+#focus app in keynumber position
+#this focus the window
+#the argument has to be the window's id
+wmctrl -i -a "$appId"
